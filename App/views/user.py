374c2b7adb3c4adf469239d325d495700aac2dff
AaronMaharaj <aaronmaharaj00@gmail.com>
2025-12-05 13:40:49 -0400

    added a basic custom homepage, no UI just an intro list of the endpoints

--- App/views/user.py
+++ App/views/user.py
@@ -12,17 +12,17 @@ from App.controllers import (
 
 user_views = Blueprint('user_views', __name__, template_folder='../templates')
 
-@user_views.route('/users', methods=['GET'])
-def get_user_page():
-    users = get_all_users()
-    return render_template('users.html', users=users)
+# @user_views.route('/users', methods=['GET'])
+# def get_user_page():
+#     users = get_all_users()
+#     return render_template('users.html', users=users)
 
-@user_views.route('/users', methods=['POST'])
-def create_user_action():
-    data = request.form
-    flash(f"User {data['username']} created!")
-    create_user(data['username'], data['password'])
-    return redirect(url_for('user_views.get_user_page'))
+# @user_views.route('/users', methods=['POST'])
+# def create_user_action():
+#     data = request.form
+#     flash(f"User {data['username']} created!")
+#     create_user(data['username'], data['password'])
+#     return redirect(url_for('user_views.get_user_page'))
 
 @user_views.route('/api/users', methods=['GET'])
 def get_users_action():
@@ -35,6 +35,6 @@ def create_user_endpoint():
     user = create_user(data['username'], data['password'])
     return jsonify({'message': f"user {user.username} created with id {user.id}"})
 
-@user_views.route('/static/users', methods=['GET'])
-def static_user_page():
-  return send_from_directory('static', 'static-user.html')
+# @user_views.route('/static/users', methods=['GET'])
+# def static_user_page():
+#   return send_from_directory('static', 'static-user.html')
